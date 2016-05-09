73f9126dfbd885ca051262102b232b10d95fb847
Bastien VINH <bus.bvinh@gmail.com>
2016-05-09 15:05:40 +0200

    Init Project [ADD] Add project from Visual Studio 2015. MVC 6 + ASP.NET 5. Compatible with mac

---
+++ src/UserAdminitration/BusinessLayer/Entity/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserAdminitration.BusinessLayer.Entity
+{
+  public interface IEntity
+  {
+
+    string ID { get; }
+
+    /// <summary>
+    /// It's a method that is need to call every time, we record the entity.
+    /// </summary>
+    void UpdateOnSave(); 
+  }
+}
---
+++ src/UserAdminitration/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace UserAdminitration.Controllers
+{
+  public class HomeController : Controller
+  {
+    // This is the only page
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Error()
+    {
+      return View();
+    }
+  }
+}
